4fca363a40bcae3bcc3cd5eb8ff88cfcc2a07d21
Exmarshall <umarhayatudeen88@gmail.com>
2025-09-17 22:16:21 +0100

    next config check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"], // allow Google avatars if you use NextAuth Google provider
+  },
 };
 
 export default nextConfig;
